8a23079501b99e9f0e92492074281fc8ecf8daac
JohnConstantion <1906172933@qq.com>
2020-11-12 15:55:08 +0800

    修改数据库的参数

--- src/main/java/io/renren/RenrenApplication.java
+++ src/main/java/io/renren/RenrenApplication.java
@@ -12,6 +12,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+/**
+ * @author constantinejohn
+ */
 @SpringBootApplication
 public class RenrenApplication {
 
